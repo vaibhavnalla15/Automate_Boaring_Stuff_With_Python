46b49e93dde3aa00a59568daec63f276a3ec7c56
vaibhav <vaibhavnalla15@gmail.com>
2025-10-06 19:27:22 +0530

    OppositeDay

--- Flow Control/oppositeday.py
+++ Flow Control/oppositeday.py
@@ -1,3 +1,18 @@
 today_is_opposite_day = True
 
-# Set 
+# Set say_it_is_opposite_day based on today_is_opposite_day
+
+if today_is_opposite_day:
+    say_it_is_opposite_day = True
+else:
+    say_it_is_opposite_day = False
+
+# If it is opposite day, toggle say_it_is_opposite_day:
+if today_is_opposite_day:
+    say_it_is_opposite_day = not say_it_is_opposite_day
+
+# Say what day it is:
+if say_it_is_opposite_day:
+    print("Today is Opposite Day.")
+else:
+    print("Today is not Opposite Day.")
